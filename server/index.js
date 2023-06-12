8babcb20518ca1aceed6ca0b41ffd6dc5a3a68ca
anshRS <anshraj0426@gmail.com>
2023-06-13 02:35:04 +0530

    Created the endpoints to register and login

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import authRoutes from './routes/auth.js'
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -25,30 +27,40 @@ app.use(cors());
 // Static Files
 app.use('/assets', express.static(path.join(process.cwd(), 'public/assets')))
 
-// File Storage 
+// File Storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  const upload = multer({ storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// Routes with Files
+app.post("/auth/register", upload.single("picture"), register);
+
+// Load Routes
+app.use('/auth', authRoutes)
 
 // Database Setup
 const PORT = process.env.PORT || '8000'
 const connectDB = async () => {
-    try {      
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser:true,
-            useUnifiedTopology: true,
-        })
-        app.listen(PORT, () => {
-            console.log(`Server listening at http://localhost:${PORT}`)
-        })
-    } catch (err) {
-        console.log(err)
-    } 
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
 connectDB();
+
+
+
+
