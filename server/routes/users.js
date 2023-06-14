a52a697fc4de3eeaa634d7f3ef7c64b78b541a16
anshRS <anshraj0426@gmail.com>
2023-06-15 00:04:11 +0530

    Developed protected endpoint for updating friends list

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, getUserFriends } from "../controllers/users.js";
+import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 // READ 
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
+
+// UPDATE 
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
