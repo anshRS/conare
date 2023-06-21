a7cd08feb95093f89ebb8309a4e01de1a0f1c5af
anshRS <anshraj0426@gmail.com>
2023-06-22 01:27:36 +0530

    Developed and tested the Responsive Navbar design

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,10 +1,220 @@
+import { useState } from "react";
+import {
+    Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery,
+} from "@mui/material";
+import {
+    Search, Message, DarkMode, LightMode, Notifications, Help, Menu, Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import CustomBox from "components/CustomBox";
 
 const Navbar = () => {
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    // const user = useSelector((state) => state.user);
+    const user = {
+        firstName: 'test',
+        lastName: 'user'
+    }
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
-}
+        <CustomBox padding="0.5rem 6%" backgroundColor={alt}>
+            <CustomBox gap="1.75rem">
+                <Typography
+                    fontWeight="bold"
+                    fontSize="clamp(1rem, 2rem, 2.25rem)"
+                    color="primary"
+                    onClick={() => navigate("/home")}
+                    sx={{
+                        "&:hover": {
+                            cursor: "pointer",
+                        },
+                    }}
+                >
+                    CONARE
+                </Typography>
+                {isNonMobileScreens && (
+                    <CustomBox
+                        backgroundColor={neutralLight}
+                        borderRadius="9px"
+                        gap="3rem"
+                        padding="0.1rem 1.5rem"
+                    >
+                        <InputBase placeholder="Search..." />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </CustomBox>
+                )}
+            </CustomBox>
+
+            {/* Desktop Navbar */}
+            {isNonMobileScreens ? (
+                <CustomBox gap="2rem">
+                    <IconButton onClick={() => dispatch(setMode())}>
+                        {theme.palette.mode === "dark" ? (
+                            <DarkMode sx={{ fontSize: "25px" }} />
+                        ) : (
+                            <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                        )}
+                    </IconButton>
+                    <Message sx={{
+                        fontSize: "25px",
+                        "&:hover": {
+                            color: theme.palette.primary.main,
+                            cursor: "pointer",
+                        },
+                    }} />
+                    <Notifications sx={{
+                        fontSize: "25px",
+                        "&:hover": {
+                            color: theme.palette.primary.main,
+                            cursor: "pointer",
+                        },
+                    }} />
+                    <Help sx={{
+                        fontSize: "25px",
+                        "&:hover": {
+                            color: theme.palette.primary.main,
+                            cursor: "pointer",
+                        },
+                    }} />
+                    <FormControl variant="standard" value={fullName}>
+                        <Select
+                            value={fullName}
+                            sx={{
+                                backgroundColor: neutralLight,
+                                width: "150px",
+                                borderRadius: "0.25rem",
+                                p: "0.25rem 1rem",
+                                "& .MuiSvgIcon-root": {
+                                    pr: "0.25rem",
+                                    width: "3rem",
+                                },
+                                "& .MuiSelect-select:focus": {
+                                    backgroundColor: neutralLight,
+                                },
+                            }}
+                            input={<InputBase />}
+                        >
+                            <MenuItem value={fullName}>
+                                <Typography>{fullName}</Typography>
+                            </MenuItem>
+                            <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+                        </Select>
+                    </FormControl>
+                </CustomBox>
+            ) : (
+                <IconButton
+                    onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                >
+                    <Menu />
+                </IconButton>
+            )}
+
+            {/* Mobile Navbar */}
+            {!isNonMobileScreens && isMobileMenuToggled && (
+                <Box
+                    position="fixed"
+                    right="0"
+                    bottom="0"
+                    height="100%"
+                    zIndex="10"
+                    maxWidth="500px"
+                    minWidth="300px"
+                    backgroundColor={background}
+                >
+
+                    <Box display="flex" justifyContent="flex-end" p="1rem">
+                        <IconButton
+                            onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                        >
+                            <Close />
+                        </IconButton>
+                    </Box>
+
+                    <CustomBox
+                        display="flex"
+                        flexDirection="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        gap="3rem"
+                    >
+                        <IconButton
+                            onClick={() => dispatch(setMode())}
+                            sx={{ fontSize: "25px" }}
+                        >
+                            {theme.palette.mode === "dark" ? (
+                                <DarkMode sx={{ fontSize: "25px" }} />
+                            ) : (
+                                <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                            )}
+                        </IconButton>
+                        <Message sx={{
+                            fontSize: "25px",
+                            "&:hover": {
+                                color: theme.palette.primary.main,
+                                cursor: "pointer",
+                            },
+                        }} />
+                        <Notifications sx={{
+                            fontSize: "25px",
+                            "&:hover": {
+                                color: theme.palette.primary.main,
+                                cursor: "pointer",
+                            },
+                        }} />
+                        <Help sx={{
+                            fontSize: "25px",
+                            "&:hover": {
+                                color: theme.palette.primary.main,
+                                cursor: "pointer",
+                            },
+                        }} />
+                        <FormControl variant="standard" value={fullName}>
+                            <Select
+                                value={fullName}
+                                sx={{
+                                    backgroundColor: neutralLight,
+                                    width: "150px",
+                                    borderRadius: "0.25rem",
+                                    p: "0.25rem 1rem",
+                                    "& .MuiSvgIcon-root": {
+                                        pr: "0.25rem",
+                                        width: "3rem",
+                                    },
+                                    "& .MuiSelect-select:focus": {
+                                        backgroundColor: neutralLight,
+                                    },
+                                }}
+                                input={<InputBase />}
+                            >
+                                <MenuItem value={fullName}>
+                                    <Typography>{fullName}</Typography>
+                                </MenuItem>
+                                <MenuItem onClick={() => dispatch(setLogout())}>
+                                    Log Out
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                    </CustomBox>
+                </Box>
+            )}
+        </CustomBox>
+    );
+};
 
 export default Navbar;
